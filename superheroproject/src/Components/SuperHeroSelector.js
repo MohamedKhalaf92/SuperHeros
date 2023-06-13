b891b81f3cc73bb31206399d73d87323bb8468f7
Mohamed Khalaf <mohamed.khalaf@infinityworks.com>
2023-06-13 15:56:55 +0100

    header footer and selector functional and styled

--- superheroproject/src/Components/SuperHeroSelector.js
+++ superheroproject/src/Components/SuperHeroSelector.js
@@ -4,32 +4,31 @@ const SuperHeroSelector = ({ superHeroStats }) => {
   const [selectedHeroe, setSelectedHeroe] = useState();
   console.log(selectedHeroe);
   return (
-    <div className="superheroselector">
-      <h1>See your favourite SuperHeroes</h1>
-      <p>Select a hero or enter any superheroe name </p>
+    <div className="superheroselector-container">
+      <h1 className="heading">See your favourite SuperHeroes</h1>
+      <p className="sub-heading">Select a hero or enter any superheroe name </p>
 
       <select
         className="selectedHeroe"
         name="Options"
         id="Options"
         onChange={(event) =>
-          setSelectedHeroe(
-            superHeroStats.filter((heroe) => {
-            return heroe.name === event.target.value
-           
-            })
-          
-          )
-         
+          superHeroStats.filter((heroe) => {
+            const name = event.target.value;
+            if (heroe.name === name) {
+              return setSelectedHeroe(name);
+            }
+          })
         }
       >
-        <option>Select Your SuperHeroe</option>
+        <option className="selectedHeroe">Select Your SuperHeroe</option>
         {superHeroStats.map((heroe) => (
-          <option key={heroe.id} value={heroe.name}>
+          <option className="selectedHeroe" key={heroe.id} value={heroe.name}>
             {heroe.name}
           </option>
         ))}
       </select>
+      <button className="power-button">Power Up!</button>
     </div>
   );
 };
